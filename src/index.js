64148a4990c346041ae20dbbc14ce12077cd9ad3
cristiandiazz <cristian.a.espin@gmail.com>
2021-08-10 12:49:15 +0100

    added apollo client and print characters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ApolloClient, ApolloProvider,InMemoryCache, HttpLink } from '@apollo/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'https://rickandmortyapi.com/graphql'
+  })
+});
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <ApolloProvider client={client}><App /></ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
